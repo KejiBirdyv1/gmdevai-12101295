b91b5f4586c165752b729d52ff4c3e3ab217997e
KejiBirdy <asuka.shigemura@benilde.edu.ph>
2023-08-12 06:32:56 +0800

    Milestone 3: A More Detailed Graph

---
+++ M3 Tank - ShigemuraAsuka/Assets/Scripts/TankManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TankManager : MonoBehaviour 
+{
+    public GameObject[] tanks;
+    private GameObject currentTank;
+
+    private void Start() 
+    {
+        SelectTank(0);
+    }
+
+    private void Update() 
+    {
+        if (currentTank == null) return;
+    }
+
+    public void TankGoToWaypoint(int waypointIndex) 
+    {
+        currentTank.GetComponent<FollowPath>().GoToWaypoint(waypointIndex);
+    }
+
+    public void SelectTank(int tankIndex) 
+    {
+        if (tankIndex >= 0 && tankIndex < tanks.Length) 
+        {
+            currentTank = tanks[tankIndex];
+        }
+    }
+}
